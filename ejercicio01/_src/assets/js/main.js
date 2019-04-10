e1d4a244e09c5735348a6efc3689224bd5ef00b9
Ines <inesp.pino@gmail.com>
2019-04-10 13:07:37 +0200

    feature: create new film and adds it to the table

--- ejercicio01/_src/assets/js/main.js
+++ ejercicio01/_src/assets/js/main.js
@@ -1,28 +1,66 @@
 'use strict';
 
-console.log('>> Ready :)');
+const jsonFilm = {
+    "filmName": "",
+    "directorName":"",
+    "idNumber": "",
+    "genre": "",
+    "publicationDate":"",
+    "grade": 5
+};
 
-//variablesGlobales = "variableGlobal";
-var irAlBarEsTbUnaVariableGlobal ="otra variable";
+const filmName = document.querySelector('#filmName');
+const directorName = document.querySelector('#directorName');
+const idNumber = document.querySelector('#idNumber');
+const genre = document.querySelector('#genre');
+const publicationDate = document.querySelector('#publicationDate');
+const grade = document.querySelector('#grade');
+const submitBtn = document.querySelector('.btn-submit');
+const myTable = document.getElementById("table-body");
 
-function miFuncion () {
-    var variableLocal = "variable local";
-    for (var i = 0; i<10; i++){
-        console.log(">>" + i);
-    }
-    console.log("El valor de" + i);
+function getFields (){
+    jsonFilm.filmName = filmName.value;
+    jsonFilm.directorName = directorName.value;
+    jsonFilm.idNumber = idNumber.value;
+    jsonFilm.genre = genre.value;
+    jsonFilm.publicationDate = publicationDate.value;
+    jsonFilm.grade = grade.value;
 }
 
-miFuncion();
+function darDeAlta (){
+    const fila = document.createElement('tr');
 
-function miClase(campo1, campo2){
-    this.clave1 = campo1;
-    this.clave2 = campo2;
+    const celdafilmName = document.createElement('td');
+    const textFilmName = document.createTextNode(jsonFilm.filmName);
+    celdafilmName.appendChild(textFilmName);
+    fila.appendChild(celdafilmName);
+
+    const celdaDirectorName = document.createElement('td');
+    const textDirectorName = document.createTextNode(jsonFilm.directorName);
+    celdaDirectorName.appendChild(textDirectorName);
+    fila.appendChild(celdaDirectorName);
+
+    const idNumber = document.createElement('td');
+    const textIdNumber = document.createTextNode(jsonFilm.idNumber);
+    idNumber.appendChild(textIdNumber);
+    fila.appendChild(idNumber);
+
+    const genre = document.createElement('td');
+    const textgenre = document.createTextNode(jsonFilm.genre);
+    genre.appendChild(textgenre);
+    fila.appendChild(genre);
+
+    const publicationDate = document.createElement('td');
+    const textPublicationDate = document.createTextNode(jsonFilm.publicationDate);
+    publicationDate.appendChild(textPublicationDate);
+    fila.appendChild(publicationDate);
+
+    const grade = document.createElement('td');
+    const textGrade = document.createTextNode(jsonFilm.grade);
+    grade.appendChild(textGrade);
+    fila.appendChild(grade);
+    
+    myTable.appendChild(fila);
 }
 
-miClase.prototype.concatena = function(){
-    return this.clave1 + this.clave2
-}
 
-var miObjeto = new miClase("primero","segundo");
-console.log(miObjeto.concatena());
