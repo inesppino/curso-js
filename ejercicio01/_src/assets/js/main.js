4279da4dfd3d852f0644813d4b5a95b6fe4130bb
Ines <inesp.pino@gmail.com>
2019-04-10 16:23:17 +0200

    feature:add film with buttons

--- ejercicio01/_src/assets/js/main.js
+++ ejercicio01/_src/assets/js/main.js
@@ -1,11 +1,13 @@
 'use strict';
 
+const Peliculas =[]
+
 const jsonFilm = {
     "filmName": "",
-    "directorName":"",
+    "directorName": "",
     "idNumber": "",
     "genre": "",
-    "publicationDate":"",
+    "publicationDate": "",
     "grade": 5
 };
 
@@ -29,6 +31,7 @@ function getFields (){
 
 function darDeAlta (){
     const fila = document.createElement('tr');
+    fila.className = jsonFilm.idNumber;
 
     const celdafilmName = document.createElement('td');
     const textFilmName = document.createTextNode(jsonFilm.filmName);
@@ -59,8 +62,44 @@ function darDeAlta (){
     const textGrade = document.createTextNode(jsonFilm.grade);
     grade.appendChild(textGrade);
     fila.appendChild(grade);
-    
+
+    const btn = document.createElement('td');
+    const btnModificar = document.createElement('button');
+    const btnTextM = document.createTextNode('Modificar');
+    btnModificar.appendChild(btnTextM);
+    btnModificar.className='btn btn-modify';
+    btnModificar.type="button";
+    btn.appendChild(btnModificar);
+
+    const btnEliminar = document.createElement('button');
+    const btnTextE = document.createTextNode('Eliminar');
+    btnEliminar.appendChild(btnTextE);
+    btnEliminar.className='btn btn-eliminar';
+    btnEliminar.type="button";
+    btnEliminar.setAttribute('dataId', jsonFilm.idNumber);
+    btn.appendChild(btnEliminar);
+
+    fila.appendChild(btn);
+
     myTable.appendChild(fila);
+
+    Peliculas.push(jsonFilm);
+    console.log(Peliculas);
+}
+
+function eliminarFila(){
+    //const seleccionado = document.querySelector(".ABC-12539-BA");
+    var guilty = document.querySelector('.btn-eliminar');
+    var seleccionado = guilty.getAttribute('data-id');
+    //myTable.removeChild(seleccionado);
+    console.log(guilty);
+    console.log(seleccionado);
 }
 
 
+// const celdaId = document.querySelector('.celdaId');
+// const celdaFilm = document.querySelector('.celdaFilm');
+// const celdaDirector = document.querySelector('.celdaId');
+// const celdaGenre = document.querySelector('.celdaId');
+// const celdaDate = document.querySelector('.celdaId');
+// const celdaBtn = document.querySelector('.celdaBtn');
